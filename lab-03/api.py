3c597061c2f479fee5eec4e8a4687f72f67adbbf
phamnguyen125rune <solarcat125@gmail.com>
2025-06-10 00:23:19 +0700

    [lab03] ecc

--- lab-03/api.py
+++ lab-03/api.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
 from cipher.rsa import RSACipher
+from cipher.ecc import ECCipher # Thêm vào đầu file
+
 
 
 app = Flask(__name__)
 
 # RSA CIPHER ALGORITHM
 rsa_cipher = RSACipher()
+ecc_cipher = ECCipher()
+
 
 
 @app.route('/api/rsa/generate_keys', methods=['GET'])
@@ -68,6 +72,29 @@ def rsa_verify_signature():
     is_verified = rsa_cipher.verify(message, signature, public_key)
     return jsonify({'is_verified': is_verified})
 
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    data = request.json
+    message = data['message']
+    private_key, __ = ecc_cipher.load_keys()
+    signature = ecc_cipher.sign(message, private_key)
+    signature_hex = signature.hex()
+    return jsonify({'signature': signature_hex})
+
+@app.route('/api/ecc/verify', methods=['POST'])
+def ecc_verify_signature():
+    data = request.json
+    message = data['message']
+    signature_hex = data['signature']
+    public_key, __ = ecc_cipher.load_keys()
+    signature = bytes.fromhex(signature_hex)
+    is_verified = ecc_cipher.verify(message, signature, public_key)
+    return jsonify({'is_verified': is_verified})
 
 # main function
 if __name__ == "__main__":
--- lab-03/cipher/ecc/ecc_cipher.py
+++ lab-03/cipher/ecc/ecc_cipher.py
@@ -8,22 +8,22 @@ class ECCipher:
         pass
 
     def generate_keys(self):
-        sk = ecdsa.SigningKey.generate() # Tạo khóa riêng tư
-        vk = sk.get_verifying_key() # Lấy khóa công khai từ khóa riêng tư
+        sk = ecdsa.SigningKey.generate()  # Tạo khóa riêng tư
+        vk = sk.get_verifying_key()  # Lấy khóa công khai từ khóa riêng tư
 
-        with open('cipher/ecc/keys/privatekey.pem', 'wb') as p:
+        with open('cipher/ecc/keys/privateKey.pem', 'wb') as p:
             p.write(sk.to_pem())
 
-        with open('cipher/ecc/keys/publickey.pem', 'wb') as p:
+        with open('cipher/ecc/keys/publicKey.pem', 'wb') as p:
             p.write(vk.to_pem())
 
         return sk, vk
 
     def load_keys(self):
-        with open('cipher/ecc/keys/privatekey.pem', 'rb') as p:
+        with open('cipher/ecc/keys/privateKey.pem', 'rb') as p:
             sk = ecdsa.SigningKey.from_pem(p.read())
 
-        with open('cipher/ecc/keys/publickey.pem', 'rb') as p:
+        with open('cipher/ecc/keys/publicKey.pem', 'rb') as p:
             vk = ecdsa.VerifyingKey.from_pem(p.read())
 
         return sk, vk
